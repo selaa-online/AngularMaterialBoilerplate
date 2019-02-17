a8febc71d52f226a1881036016365e516254766e
selakan <selakan@zone24x7.com>
2019-02-17 12:49:00 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MATRIX-FRONTEND';
+export class AppComponent  {
+  title = 'Angular Material Boilerplate';
+  panelOpenState = false;
+
+  constructor() {}
+
+
+
 }
+
+
+
